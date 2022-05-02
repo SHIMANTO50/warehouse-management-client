eac3f0fb42b81835b869c9bf7c20c0e02948f422
SHIMANTO50 <shimantotsp@gmail.com>
2022-05-02 20:25:08 +0600

    social login reset password complete

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -4,6 +4,10 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import './Login.css';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -11,6 +15,9 @@ const Login = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || "/";
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+
+    let errorElement;
     const [
         signInWithEmailAndPassword,
         user,
@@ -18,15 +25,25 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
 
+    if (error) {
 
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+
+    }
     if (user) {
         navigate(from, { replace: true });
     }
+
     const handleSubmit = event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
         signInWithEmailAndPassword(email, password);
+    }
+    const resetPassword = async () => {
+        const email = emailRef.current.value;
+        await sendPasswordResetEmail(email);
+        toast('Sent email');
     }
 
     return (
@@ -45,14 +62,16 @@ const Login = () => {
                     <Form.Label>Password</Form.Label>
                     <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
+
+                <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
+                    Login
                 </Button>
+                {errorElement}
             </Form>
             <p>New Here? <Link to='/register' className='text-primary text-decoration-none'>Please Register</Link></p>
+            <p>Forgot Password? <Button onClick={resetPassword} className='btn btn-primary'>Reset Password</Button></p>
+            <SocialLogin></SocialLogin>
+            <ToastContainer />
         </div>
     );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from './../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Register = () => {
     const navigate = useNavigate();
@@ -32,9 +33,10 @@ const Register = () => {
                 <input type="text" name="name" id="" placeholder='Your Name' />
                 <input type="email" name="email" id="" placeholder='Your Email' required />
                 <input type="password" name="password" id="" placeholder='Your Password' required />
-                <input type="submit" value="Register" />
+                <input className='w-50 mx-auto btn btn-primary' type="submit" value="Register" />
             </form>
             <p>Already Have an Account? <Link to='/login' className='text-primary text-decoration-none'>Please Login</Link></p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
