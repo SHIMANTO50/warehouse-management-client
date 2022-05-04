e590bddc23f07c37565c4122d6714c96eab2aec6
SHIMANTO50 <shimantotsp@gmail.com>
2022-05-04 21:49:57 +0600

    Delete product done

--- src/Pages/ManageInventory/ManageInventory.js
+++ src/Pages/ManageInventory/ManageInventory.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Inventory from './Inventory/Inventory';
 
 const ManageInventory = () => {
     const [products, setProducts] = useState([]);
@@ -13,15 +12,43 @@ const ManageInventory = () => {
     const handleAddProductNavigate = () => {
         navigate('/addproduct');
     }
+    const handleDelete = (id) => {
+        const proceed = window.confirm('Do you want to Delete?');
+        if (proceed) {
+            console.log('Deleting User id', id);
+            const url = `http://localhost:5000/item/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = products.filter(product => product._id !== id);
+                    setProducts(remaining);
+                })
+        }
+    }
 
     return (
         <div>
             <h2>Products: {products.length}</h2>
-            <button onClick={handleAddProductNavigate} className='btn btn-primary ms-3 mt-5'>Add New Item</button>
-            <div className="row container">
+            <button onClick={handleAddProductNavigate} className='btn btn-primary ms-3 mt-5 mb-5'>Add New Item</button>
+            <div className="container">
+
                 {
-                    products.map(product => <Inventory key={product._id} product={product}></Inventory>)
+                    products.map(product =>
+                        <div key={products._id}>
+                            <h5>Name:{product.name}</h5>
+                            <h5>Price:{product.price}</h5>
+                            <button onClick={() => handleDelete(product._id)} className='btn btn-danger mt-5'>Delete</button>
+                        </div>
+
+
+                    )
                 }
+
+
+
             </div>
         </div>
     );
