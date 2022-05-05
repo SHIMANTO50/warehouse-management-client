5064a3d7e05b9c570a6fe1e2f0915d9f27c26725
SHIMANTO50 <shimantotsp@gmail.com>
2022-05-06 03:24:49 +0600

    My Items delete done

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -12,9 +12,45 @@ const MyItems = () => {
             .then(res => res.json())
             .then(data => setMyItems(data))
     }, [])
+    const handleDelete = (id) => {
+        const proceed = window.confirm('Do you want to Delete?');
+        if (proceed) {
+            console.log('Deleting User id', id);
+            const url = `http://localhost:5000/myitem/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = myitems.filter(product => product._id !== id);
+                    setMyItems(remaining);
+                })
+        }
+    }
     return (
         <div>
             <h2>Total Items:{myitems.length}</h2>
+            <div className="container product-grid">
+
+                {
+                    myitems.map(product =>
+                        <div className='border-design p-5' key={product._id}>
+                            <h5>Name:{product.prname}</h5>
+                            <h5>Price:{product.prprice}</h5>
+                            <p>Quantity: {product.prquantity} kg</p>
+                            <p>Other Description: {product.prdescription}</p>
+                            <p>SupplierName: {product.prsupplierName}</p>
+                            <button onClick={() => handleDelete(product._id)} className='btn btn-danger mt-5'>Delete</button>
+                        </div>
+
+
+                    )
+                }
+
+
+
+            </div>
 
         </div>
     );
